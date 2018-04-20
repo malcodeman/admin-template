a0fa049f191239c6423c2d66be46cad601cd0969
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-04-20 15:05:57 +0200

    Adds brands new form styling

---
+++ src/Core/styles/constants.js
@@ -0,0 +1,13 @@
+export const Color = {
+  /* Brand */
+  primary: "rgba(0, 0, 0, .8)",
+  secondary: "rgba(0, 0, 0, .6)",
+  green: "#1dba53",
+  red: "#ef456a",
+  yellow: "#FFAB02",
+
+  /* Misc */
+  white: "#fff",
+  black: "#000",
+  facebook: "#36559d"
+};
--- src/Features/Brands/components/BrandsNew.js
+++ src/Features/Brands/components/BrandsNew.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
+
 import { requestBrands, uploadBrands } from "../actions/actions_brands";
 import BrandsNewFrom from "./BrandsNewForm";
+import Header from "../../Layout/Header";
+import companyLogo from "../images/ic_header_company.png";
+
+const Wrapper = styled.div`
+  padding 20px;
+`;
 
 class BrandsNew extends Component {
   render() {
     return (
-      <div>
-        brands new<BrandsNewFrom uploadBrands={this.props.uploadBrands} />
-      </div>
+      <Wrapper>
+        <Header logo={companyLogo} text="New Brand" company="Sony Inc." />
+        <BrandsNewFrom uploadBrands={this.props.uploadBrands} />
+      </Wrapper>
     );
   }
 }
