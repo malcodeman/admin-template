b01a071b618dd90f2b2e3d06539ee8aacc018b9b
malcodeman <26983729+malcodeman@users.noreply.github.com>
2018-04-23 17:25:11 +0200

    New campaign reducer.

--- src/Features/Campaigns/reducers/reducers_campaigns.js
+++ src/Features/Campaigns/reducers/reducers_campaigns.js
@@ -1,8 +1,14 @@
-import { REQUEST_CAMPAIGNS } from "../actions/actions_campaigns";
-import { RECIVE_CAMPAIGNS } from "../actions/actions_campaigns";
+import {
+  REQUEST_CAMPAIGNS,
+  RECIVE_CAMPAIGNS,
+  UPLOAD_CAMPAIGNS_SUCCEEDED,
+  UPLOAD_CAMPAIGNS_FAILED,
+  UPLOAD_PRODUCT_IMAGE_SUCCEEDED
+} from "../actions/actions_campaigns";
 
 const initialState = {
   loading: true,
+  uploadCampaignsFailed: false,
   campaigns: []
 };
 
@@ -17,6 +23,28 @@ export default (state = initialState, action) => {
         ...state,
         loading: false,
         campaigns: action.payload
+      };
+    case UPLOAD_CAMPAIGNS_SUCCEEDED:
+      return {
+        ...state,
+        loading: false,
+        campaigns: [...state.campaigns, action.payload]
+      };
+    case UPLOAD_CAMPAIGNS_FAILED:
+      return {
+        ...state,
+        uploadCampaignsFailed: true
+      };
+    case UPLOAD_PRODUCT_IMAGE_SUCCEEDED:
+      return {
+        ...state,
+        // Find last item in array and change it's product image with just uploaded product image
+        campaigns: state.campaigns.map(
+          (campaign, index) =>
+            index === state.campaigns.length - 1
+              ? { ...campaign, imageUrl: action.payload }
+              : campaign
+        )
       };
     default:
       return state;
