55696474a58319d209927ef6e43ae07b675d5cde
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-01-24 17:19:01 +0100

    Adds mobile filters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Grid, Box, Divider, Heading, Center } from "@chakra-ui/layout";
+import { Grid, Box, Divider, Heading, Center, Flex } from "@chakra-ui/layout";
 import { Spinner } from "@chakra-ui/spinner";
+import { Button } from "@chakra-ui/button";
+import { useDisclosure } from "@chakra-ui/hooks";
 import { map } from "ramda";
+import { Filter } from "react-feather";
 import type { NextPage } from "next";
 
 import useAssets from "../hooks/useAssets";
@@ -9,6 +12,7 @@ import useCollections from "../hooks/useCollections";
 
 import Asset from "../components/Asset";
 import Filters from "../components/Filters";
+import Drawer from "../components/Drawer";
 
 import { OrderBy, OrderDirection } from "../types";
 
@@ -22,50 +26,70 @@ const Home: NextPage = () => {
     collection: "azuki",
   });
   const { data: collections, isLoading } = useCollections();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Grid gridGap={"4"} gridTemplateColumns={["1fr", "1fr", "256px 1fr"]}>
-      <Box display={["none", "none", "block"]}>
-        <Heading fontSize={"2xl"} textTransform={"uppercase"} mb={"2"}>
-          Filter
-        </Heading>
-        <Divider mb={"4"} />
+    <React.Fragment>
+      <Grid gridGap={"4"} gridTemplateColumns={["1fr", "1fr", "256px 1fr"]}>
+        <Box display={["none", "none", "block"]}>
+          <Heading fontSize={"2xl"} textTransform={"uppercase"} mb={"2"}>
+            Filter
+          </Heading>
+          <Divider mb={"4"} />
+          <Filters
+            collections={collections}
+            setOrderBy={setOrderBy}
+            setOrderDirection={setOrderDirection}
+          />
+        </Box>
+        <Box>
+          <Flex justifyContent={"space-between"}>
+            <Heading fontSize={"2xl"} textTransform={"uppercase"} mb={"2"}>
+              NFTs
+            </Heading>
+            <Button
+              display={["inline-flex", "inline-flex", "none"]}
+              size={"sm"}
+              onClick={onOpen}
+              leftIcon={<Filter />}
+            >
+              Filters
+            </Button>
+          </Flex>
+          <Divider mb={"4"} />
+          {isLoading ? (
+            <Center>
+              <Spinner />
+            </Center>
+          ) : (
+            <Grid
+              gridGap={"4"}
+              gridTemplateColumns={"repeat(auto-fill, minmax(222px, 1fr))"}
+            >
+              {map(
+                (item) => (
+                  <Asset
+                    key={item.id}
+                    id={item.id}
+                    image_url={item.image_url}
+                    name={item.name}
+                    permalink={item.permalink}
+                  />
+                ),
+                assets.assets
+              )}
+            </Grid>
+          )}
+        </Box>
+      </Grid>
+      <Drawer isOpen={isOpen} header="Filters" onClose={onClose}>
         <Filters
           collections={collections}
           setOrderBy={setOrderBy}
           setOrderDirection={setOrderDirection}
         />
-      </Box>
-      <Box>
-        <Heading fontSize={"2xl"} textTransform={"uppercase"} mb={"2"}>
-          NFTs
-        </Heading>
-        <Divider mb={"4"} />
-        {isLoading ? (
-          <Center>
-            <Spinner />
-          </Center>
-        ) : (
-          <Grid
-            gridGap={"4"}
-            gridTemplateColumns={"repeat(auto-fill, minmax(222px, 1fr))"}
-          >
-            {map(
-              (item) => (
-                <Asset
-                  key={item.id}
-                  id={item.id}
-                  image_url={item.image_url}
-                  name={item.name}
-                  permalink={item.permalink}
-                />
-              ),
-              assets.assets
-            )}
-          </Grid>
-        )}
-      </Box>
-    </Grid>
+      </Drawer>
+    </React.Fragment>
   );
 };
 
